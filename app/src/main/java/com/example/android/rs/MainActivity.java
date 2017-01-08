73930c17cc2dd298e839ac4f86297bce507c08fb
zaiyong <zaiyongs@gmail.com>
2017-01-08 18:44:21 +0800

    sdg

--- app/src/main/java/com/example/android/rs/MainActivity.java
+++ app/src/main/java/com/example/android/rs/MainActivity.java
@@ -26,8 +26,11 @@ import android.support.v8.renderscript.Element;
 import android.support.v8.renderscript.RenderScript;
 import android.support.v8.renderscript.ScriptIntrinsic3DLUT;
 import android.support.v8.renderscript.Type;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import com.abcew.camera.ScriptC_image_translate_3d;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -49,7 +52,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             R.drawable.lut_bw,
             R.drawable.lut_celsius,
             R.drawable.lut_chest,
-            R.drawable.lut_breeze
+            R.drawable.lut_breeze,
+            R.drawable.lut_sin
     };
 
     @Override
@@ -78,6 +82,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             int w, h;
             int[] lut;
 
+
+            for(int y=0;y<512;y++){
+
+
+                for(int x=0;x<512;x++){
+
+                     if(y<3){
+                         int timex=x/64;
+
+                         int remainderx=x%64;
+
+                         int timey=y/8;
+                         int remaindery=y%8;
+
+                         Log.i("tagtimex","timex"+timex+"remainderx"+remainderx+"timey"+timey+"remaindery"+remaindery);
+                         int count=(remainderx*64+timex + remaindery*8+timey*512);
+                         Log.i("tagtimex","timex"+count);
+
+                         Log.i("tagtimex","timex"+count%512+"/"+count/512);
+                     }
+
+
+                }
+
+            }
+
             if (mScriptlut == null) {
                 mScriptlut = ScriptIntrinsic3DLUT.create(mRs, Element.U8_4(mRs));
             }
@@ -90,34 +120,108 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mAllocIn = Allocation.createFromBitmap(mRs, mBitmap);
                 mAllocOut = Allocation.createFromBitmap(mRs, mOutputBitmap);
             }
+            long start = System.currentTimeMillis();
+            Type.Builder tb = new Type.Builder(mRs, Element.U8_4(mRs));
+            tb.setX(64).setY(64).setZ(64);
+            Type t = tb.create();
+            mAllocCube = Allocation.createTyped(mRs, t);
             if (mFilter != 0) {
+
                 mLutBitmap = BitmapFactory.decodeResource(getResources(), mLut3D[mFilter - 1]);
-                w = mLutBitmap.getWidth();
-                h = mLutBitmap.getHeight();
+                start = System.currentTimeMillis();
+//                w = mLutBitmap.getWidth();
+//                h = mLutBitmap.getHeight();
 //                redDim = w / h;
 //                greenDim = redDim;
 //                blueDim = redDim;
 
-                redDim = greenDim = blueDim = 64;
-                int[] pixels = new int[w * h];
-                lut = new int[w * h];
-                mLutBitmap.getPixels(pixels, 0, w, 0, 0, w, h);
 
-                int i = 0;
+//                redDim = greenDim = blueDim = 64;
+//                int[] pixels = new int[w * h];
+//                lut = new int[w * h];
+//                mLutBitmap.getPixels(pixels, 0, w, 0, 0, w, h);
+//
+//                int i = 0;
                 //512*512的查色表，根据b的大小，划分 8*8的小块
-                for (int r=0; r<redDim; r++){
-                    for (int g=0; g<greenDim; g++){
-                        for (int b=0; b<blueDim; b++){
+//                for (int r = 0; r < redDim; r++) {
+//                    for (int g = 0; g < greenDim; g++) {
+//                        for (int b = 0; b < blueDim; b++) {
+//
+//                            int blockX = b % 8;
+//                            int blockY = b / 8;
+//
+////                            int c = (blockY * 64 + g) * 512 + (blockX * 64 + r);
+//                            lut[i++] = pixels[(blockY * 64 + g) * 512 + (blockX * 64 + r)];
+//                            if(g<1){
+//                                Log.i("block", (blockY * 64 + g) * 512 + (blockX * 64 + r)+"");
+//
+//                            }
+//
+//                        }
+//                    }
+//                }
 
-                            int blockX = b%8;
-                            int blockY = b/8;
+//                for (int b = 0; b < redDim; b++) {
+//                    for (int  g= 0; g < greenDim; g++) {
+//                        for (int a = 0; a < blueDim; a++) {
+//
+//                            int v = 0xff000000;
+//                            v |= (0xff * a / (redDim - 1));
+//                            v |= (0xff * g / (redDim - 1)) << 8;
+//                            v |= (0xff * b / (redDim - 1)) << 16;
+//                            lut[b*redDim*redDim + g*redDim + a] = v;
+//
+//
+//                            int blockX = b % 8;
+//                            int blockY = b / 8;
+//
+////                            int c = (blockY * 64 + g) * 512 + (blockX * 64 + r);
+//                            lut[i++] = pixels[(blockY * 64 + g) * 512 + (blockX * 64 + r)];
+//                            if(g<2){
+//                                Log.i("block", "blockX" + blockX + "Y" + blockY + "blockZ" + b);
+//
+//                            }
+//
+//                        }
+//                    }
+//                }
 
-                            lut[i++] = pixels[(blockY * 64 + g) * 512 + (blockX * 64 + r)];
-                        }
-                    }
-                }
+//                for (int z = 0; z < sz; z++) {
+//                    for (int y = 0; y < sy; y++) {
+//                        for (int x = 0; x < sx; x++ ) {
+//                            int v = 0xff000000;
+//                            v |= (0xff * x / (sx - 1));
+//                            v |= (0xff * y / (sy - 1)) << 8;
+//                            v |= (0xff * z / (sz - 1)) << 16;
+//                            dat[z*sy*sx + y*sx + x] = v;
+//                        }
+//                    }
+////                }
+                Bitmap cache = Bitmap.createBitmap(512, 512, Bitmap.Config.ARGB_8888);
+
+                Allocation mAllocIn = Allocation.createFromBitmap(mRs, mLutBitmap);
+                Allocation mAllocOut = Allocation.createFromBitmap(mRs, cache);
+
+
+//                Allocation mAllocCube = Allocation.createTyped(mRs, tb.create());
+
+                ScriptC_image_translate_3d script = new ScriptC_image_translate_3d(mRs);
+                script.set_gIn(mAllocIn);
+                script.set_gOut(mAllocOut);
+//                script.set_rsAllocationIn(mAllocIn);
+//                script.set_rsAllocationOut(mAllocOut);
+                //Invoke script
+                script.forEach_root(mAllocIn, mAllocOut);
+
+                byte[] lut2 = new byte[512 * 512 * 4];
+                mAllocOut.copyTo(lut2);
+
+
+//                mAllocOut.copyTo(bitmap);
+                mAllocCube.copyFromUnchecked(lut2);
 
             } else {
+                start = System.currentTimeMillis();
                 // identity filter provided for refrence
                 redDim = greenDim = blueDim = 64;
                 int i = 0;
@@ -134,17 +238,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         }
                     }
                 }
+
+                mAllocCube.copyFromUnchecked(lut);
             }
-            Type.Builder tb = new Type.Builder(mRs, Element.U8_4(mRs));
-            tb.setX(redDim).setY(greenDim).setZ(blueDim);
-            Type t = tb.create();
-            mAllocCube = Allocation.createTyped(mRs, t);
-            mAllocCube.copyFromUnchecked(lut);
+            long end2 = System.currentTimeMillis();
+            Log.i("ScriptIntrinsic3DLUT", "处理时间：" + (end2 - start) + "毫秒");
+
+
+
 
             mScriptlut.setLUT(mAllocCube);
             mScriptlut.forEach(mAllocIn, mAllocOut);
 
             mAllocOut.copyTo(mOutputBitmap);
+
+            long end = System.currentTimeMillis();
+            Log.i("ScriptIntrinsic3DLUT", "运行时间：" + (end - start) + "毫秒");
             return null;
         }
 
---
+++ app/src/main/rs/image_translate_3d.rs
@@ -0,0 +1,18 @@
+#pragma version(1)
+#pragma rs java_package_name(com.abcew.camera)
+#pragma rs_fp_relaxed
+
+uchar alpha;
+rs_allocation gIn;// 输入图像的 allocation 对象
+rs_allocation gOut;// 输出图像的 allocation 对象
+rs_script gScript;// RenderScript 对象
+
+uchar4 __attribute__((kernel)) root(uchar4 in,uint32_t x, uint32_t y) {
+    uint32_t timex=x/64;
+    uint32_t remainderx=x%64;
+    uint32_t timey=y/8;
+    uint32_t remaindery=y%8;
+    uint32_t count=  remainderx*64+timex + remaindery*8+timey*512;
+
+    return rsGetElementAt_uchar4(gIn, count%512, count/512);
+}
